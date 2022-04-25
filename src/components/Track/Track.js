01501fee7f1921e963237c9c74ab5d6073681700
guilhermodaguiar <dguilhermo@gmail.com>
2022-04-25 14:02:07 +0200

    all-components-js-and-css

---
+++ src/components/Track/Track.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Track.css';
+
+class Track extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+renderAction(){
+    if(this.props.isRemoval) {
+     return <a className="Track-action">-</a>
+    }
+    return <a className="Track-action">+</a>
+}
+
+    render() {
+        return(
+            <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                <button className="Track-action"><!-- + or - will go here --></button>
+            </div>
+        );
+    }
+}
+
+export default Track;
