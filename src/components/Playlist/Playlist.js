13708d7c89adb73e6da44bd3db2ed5c2fcf97aec
guilhermodaguiar <dguilhermo@gmail.com>
2022-04-30 15:45:36 +0200

    commit 30 april

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,16 +3,33 @@ import './Playlist.css';
 
 import TrackList from "../TrackList/TrackList";
 
-class Playlist extends React.Component{
+class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.handleNameChange=this.handleNameChange.bind(this);
+    }
 
+handleNameChange(e) {
+    this.props.onNameChange(e.target.value);
+    }
 
     render() {
-        return(
+        return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'}/>
-                <TrackList />
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <input
+                    defaultValue={'New Playlist'}
+                    onChange={this.handleNameChange}
+                />
+                <TrackList
+                    tracks={this.props.playlistTracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={true}
+                />
+                <button
+                    className="Playlist-save"
+                    onClick={this.props.onSave}
+                >SAVE TO SPOTIFY</button>
             </div>
         );
     }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,14 +4,14 @@ import './SearchResults.css';
 import TrackList from "../TrackList/TrackList";
 
 class SearchResults extends React.Component{
-
-
-
     render() {
         return(
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList />
+                <TrackList
+                    tracks={this.props.searchResults}
+                    onAdd={this.props.onAdd}
+                />
             </div>
         );
     }
